26d0ebff440590e439427dc1e174974fbe90ab15
Collin Fair <cpf@cpfx.ca>
2016-08-16 21:01:04 -0400

    Make write_utf8 not totally broken

--- runtime/utf8.c
+++ runtime/utf8.c
@@ -21,15 +21,15 @@ void write_utf8(char* ptr, uint16_t codept) {
         *ptr = codept;
     } else if (codept <= 0x7ff) {
         *(ptr++) = 0b11000000 | (codept >> 6);
-        *(ptr) = 0b10000000 | (codept);
+        *(ptr) = 0b10000000 | (codept & 0b111111);
     } else if (codept <= 0xFFFF) {
-        *(ptr++) = 0b11100000 | (codept >> 12);
-        *(ptr++) = 0b10000000 | (codept >> 6);
-        *(ptr) = 0b10000000 | (codept);
+        *(ptr++) = 0b11100000 | ((codept >> 12) & 0b111111);
+        *(ptr++) = 0b10000000 | ((codept >> 6) & 0b111111);
+        *(ptr) = 0b10000000 | (codept & 0b111111);
     } else {
-        *(ptr++) = 0b11110000 | (codept >> 18);
-        *(ptr++) = 0b10000000 | (codept >> 12);
-        *(ptr++) = 0b10000000 | (codept >> 6);
-        *(ptr) = 0b10000000 | (codept);
+        *(ptr++) = 0b11110000 | ((codept >> 18) & 0b111111);
+        *(ptr++) = 0b10000000 | ((codept >> 12) & 0b111111);
+        *(ptr++) = 0b10000000 | ((codept >> 6) & 0b111111);
+        *(ptr) = 0b10000000 | (codept & 0b111111);
     }
 }
