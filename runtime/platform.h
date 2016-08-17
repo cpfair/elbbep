70d6d94c89ffb40033c531f2594e0024c65e5462
Collin Fair <cpf@cpfx.ca>
2016-08-17 11:56:49 -0400

    Fix notification pop-ups not getting transformed due to falling in CCM instead of SRAM1/2.

--- runtime/platform.h
+++ runtime/platform.h
@@ -1,2 +1,6 @@
-#define SRAM_BASE 0x20000000
+// This isn't actually the start of SRAM1 - it's the start of core-coupled SRAM.
+// Notification popups have strings in this area.
+// I don't know if the STM32F3 has CCM, but it doesn't really matter.
+// These values just ensure we don't go writing to the firmware in flash.
+#define SRAM_BASE 0x10000000
 #define SRAM_EXTENT 0x2001FFFF
