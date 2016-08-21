c19fabebce341be174f44bbc0e28c4be32c1c091
Collin Fair <cpf@cpfx.ca>
2016-08-21 14:04:24 -0400

    Rewrite utf8 read - a 50 byte savings!

--- runtime/utf8.c
+++ runtime/utf8.c
@@ -2,14 +2,23 @@
 
 uint16_t read_utf8(char **ptr) {
   uint16_t codept = **ptr;
-  if ((codept & 0b11000000) == 0b11000000) {
-    codept = ((codept & ~0b11000000) << 6) | (*(++(*ptr)) & ~0b10000000);
-  } else if ((codept & 0b11100000) == 0b11100000) {
-    codept = ((codept & ~0b11100000) << 6) | (*(++(*ptr)) & ~0b10000000);
-    codept = (codept << 6) | (*(++(*ptr)) & ~0b10000000);
-  } else if ((codept & 0b11110000) == 0b11110000) {
-    codept = ((codept & ~0b11110000) << 6) | (*(++(*ptr)) & ~0b10000000);
-    codept = (codept << 6) | (*(++(*ptr)) & ~0b10000000);
+  int ct = 0;
+  switch (codept >> 4) {
+  case 0xF: // 0b1111
+      // 4 bytes.
+      ct = 3;
+      break;
+  case 0xE: // 0b1110
+      // 3 bytes.
+      ct = 2;
+      break;
+  case 0xC: // 0b1100
+  case 0xD: // 0b1101
+      // 2 bytes.
+      ct = 1;
+  }
+  codept &= (~0b11110000000) >> ct;
+  while (ct--) {
     codept = (codept << 6) | (*(++(*ptr)) & ~0b10000000);
   }
   (*ptr)++;
